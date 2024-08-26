d99170bcf8e6980e31ac50605ae1f8671a3a9102
Himangi Tripathy <123282807+H1mang1@users.noreply.github.com>
2024-08-26 14:33:44 +0000

    new repo

--- frontend/src/components/Sidebar.jsx
+++
@@ -1,9 +0,0 @@
-const Sidebar=()=>{
-    return(
-        <aside>
-            <h1>Welcome to MindMend</h1>
-            <p>An Accessible Health Platform for Everyone</p>
-        </aside>
-    );
-};
-export default Sidebar;
